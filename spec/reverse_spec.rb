50dc4b424ffc8ef752288a067885084ac6a887df
josuerushanika <josuerushanika@gmail.com>
2023-05-11 22:48:52 +0200

    Fix reverse test

--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -1,12 +1,13 @@
 require './solver'
 
 describe Solver do
-    context 'Testing reverse method' do
-        solver = Solver.new
-        it 'reverses the given input' do
-            expect(solver.reverse('hello')).to eql('olleh')
-            expect(solver.reverse('obed')).to eql('obed')
-            expect(solver.reverse('josue')).to eql('josue')
-        end    
-    end    
-end    
+  context 'Testing reverse method' do
+    solver = Solver.new
+
+    it 'reverses the given input' do
+      expect(solver.reverse('hello')).to eql('olleh')
+      expect(solver.reverse('obed')).to eql('debo')
+      expect(solver.reverse('josue')).to eql('eusoj')
+    end
+  end
+end
