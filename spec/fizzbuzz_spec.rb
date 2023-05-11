710ddc97e5e262f252ac2cf1b8dd8cbb91965069
josuerushanika <josuerushanika@gmail.com>
2023-05-11 22:39:13 +0200

    Update some

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,5 +12,12 @@ describe Solver do
             expect(solver.fizzbuzz(20)).to eql('bizz')
         end
 
+        it 'should return "fizzbuzz" for 15' do
+            expect(solver.fizzbuzz(30)).to eql('fizzbuzz')
+        end
+
+        it 'should return "22" for 22' do
+            expect(solver.fizzbuzz(22)).to eql('22')
+        end
     end    
 end
--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -4,7 +4,8 @@ describe Solver do
     context 'Testing reverse method' do
         solver = Solver.new
         it 'reverses the given input' do
-           
+            expect(solver.reverse('hello')).to eql('olleh')
+            expect(solver.reverse('obed')).to eql('obed')
             expect(solver.reverse('josue')).to eql('josue')
         end    
     end    
